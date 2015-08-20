05bf6410fba22d7bc581e91988221c3ed6d14f26
Jeremy Pinnix <jpinnix@pixelgrazer.com>
2015-08-20 14:05:19 -0500

    Adding the downloadable files

---
+++ karabiner-import.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set private.swap_space_and_tab 1
+/bin/echo -n .
+$cli set remap.optionrcommandr 1
+/bin/echo -n .
+/bin/echo
